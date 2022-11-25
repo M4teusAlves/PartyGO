ef00876772cc87a109919118922bf7ac11095e9e
M4teusAlves <mateus.alves.desenvolvedor@gmail.com>
2022-11-25 17:07:54 -0300

    25/11 17:07 Detalhes: Adição da página home e eventos

---
+++ assets/js/home.js
@@ -0,0 +1,6 @@
+
+var url = '<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d30038.63942641771!2d-47.93630720800487!3d-19.762379097297924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sfesta!5e0!3m2!1spt-BR!2sbr!4v1669405188966!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><div id="resultado"><a href="./eventos.html">^</a><div><img src="./assets/img/casa.png" alt="casa" width="25%"><p>Evento 1</p></div></div>'
+
+function go(){
+    document.getElementById('principal').innerHTML = url
+}
--- assets/js/login.js
+++
@@ -1,7 +0,0 @@
-let btn = document.getElementsByTagName('button')
-
-function entrar(){
-    window.location.href = './home.html'
-}
-
-btn.addEventListener("click",entrar)
