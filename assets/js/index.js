98d57abf3a08ec77009d4c2545b2d472c6b98e7e
M4teusAlves <mateus.alves.desenvolvedor@gmail.com>
2022-11-24 23:10:37 -0300

    Primeiro commit

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+let flag = 0
+
+function alterar_url(){
+    window.location.href = './login.html'
+}
+
+setInterval(alterar_url,2500)
+
---
+++ assets/js/login.js
@@ -0,0 +1,7 @@
+let btn = document.getElementsByTagName('button')
+
+function entrar(){
+    window.location.href = './home.html'
+}
+
+btn.addEventListener("click",entrar)
